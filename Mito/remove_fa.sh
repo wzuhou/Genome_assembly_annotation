c0e3d02380f27dcd0f8f8df283d26ca8a0e9b784
wzuhou <wuz1992@hotmail.com>
2022-04-22 12:08:32 +0100

    Create remove_fa.sh
    first try

---
+++ Mito/remove_fa.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Grid Engine options
+#$ -N mito
+#$ -cwd
+#$ -l h_vmem=8G
+#$ -M zwu33@ed.ac.uk
+#$ -m baes
+#$ -P roslin_smith_grp
+#'$ -pe sharedmem 8
+
+# If you plan to load any software modules, then you must first initialise the modules framework.
+. /etc/profile.d/modules.sh
+# Choose the staging environment
+#export OMP_NUM_THREADS=$NSLOTS
+
+
+#Mito mapping
+#module load roslin/samtools/1.10
+LALOfasta=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/RNA_LALO/mhindle/LALO_RNASeq_newgenome/LALO_new_fasta/LALO.fasta
+date
+Rm_scaffold=mito_out2.paf.sort.query.scaffold.header
+#this code causes format is shifted
+#awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' ${LALOfasta} | grep -Ff ${Rm_scaffold} -  > ${LALOfasta}_rm_MT.fa
+MT_seq=${LALOfasta}_MT_like.fa
+grep -v -Ff ${MT_seq} ${LALOfasta}>${LALOfasta}_rm_MT.fa
