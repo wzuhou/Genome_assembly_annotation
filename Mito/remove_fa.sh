babdfa5d26095ebb35e0369bef46b9f0579d3124
wzuhou <wuz1992@hotmail.com>
2022-04-22 12:10:40 +0100

    Update remove_fa.sh

--- Mito/remove_fa.sh
+++ Mito/remove_fa.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-
+###########################################
+#Mitochondiral contig removal             #
+#Author:Zhou Wu                           #
+#Date:2022-Apr                            #
+###########################################
 # Grid Engine options
 #$ -N mito
 #$ -cwd
@@ -20,7 +24,7 @@
 LALOfasta=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/RNA_LALO/mhindle/LALO_RNASeq_newgenome/LALO_new_fasta/LALO.fasta
 date
 Rm_scaffold=mito_out2.paf.sort.query.scaffold.header
-#this code causes format is shifted
+#this command becauses format is shifted
 #awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' ${LALOfasta} | grep -Ff ${Rm_scaffold} -  > ${LALOfasta}_rm_MT.fa
 MT_seq=${LALOfasta}_MT_like.fa
 grep -v -Ff ${MT_seq} ${LALOfasta}>${LALOfasta}_rm_MT.fa
